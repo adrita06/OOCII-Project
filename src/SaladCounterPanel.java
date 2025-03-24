0f87e5c7368f84e83dfc013e82d59ceef83b42ed
adrita06 <adritaahsan06@gmail.com>
2025-03-24 21:04:08 +0600

    Project initiation

---
+++ src/SaladCounterPanel.java
@@ -0,0 +1,6 @@
+public class SaladCounterPanel {
+
+    public static void main(String[] args) {
+        System.out.println("1.Menu\n2.Customize Salad\n3.Popular Picks\4.Admin Settings(for admins only)");
+    }
+}
